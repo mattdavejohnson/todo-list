d870b473219b5e87f455cd2bf7e9313a86c24186
Matthew Johnson <matthew.dave.johnson@gmail.com>
2022-07-20 14:57:38 -0700

    Todo class extracted into its own file

--- todolist.js
+++ todolist.js
@@ -1,37 +1,4 @@
-// Project contains two classes:
-// TodoList class: represents a collection (array) of Todo objects
-// Todo class: represents a todo item and its associated data
-
-class Todo {
-  static DONE_MARKER = 'X';
-  static UNDONE_MARKER = ' ';
-
-  constructor(title) {
-    this.title = title;
-    this.done = false;
-  }
-
-  toString() {
-    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
-    return `[${marker}] ${this.title}`;
-  }
-
-  markDone() {
-    this.done = true;
-  }
-
-  markUndone() {
-    this.done = false;
-  }
-
-  isDone() {
-    return this.done;
-  }
-
-  getTitle() {
-    return this.title;
-  }
-}
+const Todo = require('./todo.js');
 
 class TodoList {
   constructor(title) {
@@ -39,12 +6,12 @@ class TodoList {
     this.todos = [];
   }
 
-  add(item) {
-    if (!(item instanceof Todo)) {
+  add(todo) {
+    if (!(todo instanceof Todo)) {
       throw new TypeError('can only add Todo objects');
     }
 
-    this.todos.push(item);
+    this.todos.push(todo);
   }
 
   size() {
@@ -89,12 +56,6 @@ class TodoList {
     return this.todos.splice(index, 1);
   }
 
-  _validateIndex(index) {
-    if (!(index in this.todos)) {
-      throw new ReferenceError(`invalid index: ${index}`);
-    }
-  }
-
   toString() {
     let title = `---- ${this.title} ----`;
     let list = this.todos.map((todo) => todo.toString()).join('\n');
@@ -102,7 +63,7 @@ class TodoList {
   }
 
   forEach(callback) {
-    this.todos.forEach(callback);
+    this.todos.forEach((todo) => callback(todo));
   }
 
   filter(callback) {
@@ -129,9 +90,9 @@ class TodoList {
   }
 
   markDone(title) {
-    let selection = this.findByTitle(title);
-    if (selection !== undefined) {
-      selection.markDone();
+    let todo = this.findByTitle(title);
+    if (todo !== undefined) {
+      todo.markDone();
     }
   }
 
@@ -146,80 +107,12 @@ class TodoList {
   toArray() {
     return this.todos.slice();
   }
+
+  _validateIndex(index) {
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
 }
 
-let list = new TodoList("Today's Todos");
-
-let todo1 = new Todo('Buy milk');
-let todo2 = new Todo('Clean room');
-let todo3 = new Todo('Go to the gym');
-let todo4 = new Todo('Go shopping');
-let todo5 = new Todo('Feed the cats');
-let todo6 = new Todo('Study for Launch School');
-
-list.add(todo1);
-list.add(todo2);
-list.add(todo3);
-list.add(todo4);
-list.add(todo5);
-list.add(todo6);
-
-console.log(list.toArray());
-
-// list.markAllDone();
-// console.log(list);
-
-// list.markAllUndone();
-// console.log(list);
-
-// todo1.markDone();
-// todo2.markDone();
-
-// console.log(list.allDone());
-// console.log(list.allNotDone());
-
-// let doneTodos = list.filter((todo) => todo.isDone());
-// console.log(doneTodos);
-
-// console.log(list.filter((todo) => todo.isDone()).first());
-
-// list.forEach((todo) => console.log(todo.toString()));
-
-// console.log(list.first());
-// console.log(list.last());
-
-// let emptyList = new TodoList('Empty List');
-// console.log(emptyList.first());
-// console.log(emptyList.last());
-
-// console.log(list.itemAt(1));
-
-// list.markDoneAt(1);
-// console.log(list);
-
-// list.markUndoneAt(1);
-// console.log(list);
-
-// console.log(list.isDone());
-
-// list.markDoneAt(0);
-// list.markDoneAt(1);
-// list.markDoneAt(2);
-// list.markDoneAt(3);
-// console.log(list.isDone());
-
-// list.markUndoneAt(2);
-// console.log(list.isDone());
-
-// console.log(list.shift());
-// console.log(list.pop());
-// console.log(list);
-
-// console.log(emptyList.shift());
-// console.log(emptyList.pop());
-// console.log(emptyList);
-
-// console.log(list.removeAt(2));
-// console.log(list.removeAt(0));
-// console.log(list.removeAt(1));
-// console.log(list);
+module.exports = TodoList;
